372b18bcdc190854dcf32f620b9e6ee4aa435ffa
sebastientromp <sebastien.tromp@gmail.com>
2026-09-25 12:04:00 +0200

    allow unsquelch

--- Plugin.cs
+++ Plugin.cs
@@ -1,7 +1,9 @@
 ﻿using BepInEx;
 using BepInEx.Logging;
 using HarmonyLib;
-using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
 using BepInEx.Configuration;
 
 namespace FirestoneAutoSquelch
@@ -35,16 +37,116 @@ namespace FirestoneAutoSquelch
         }
     }
 
-    [HarmonyPatch(typeof(EnemyEmoteHandler), "IsSquelched", new Type[] { typeof(int) })]
+    [HarmonyPatch(typeof(EnemyEmoteHandler))]
     public static class SquelchPatcher
     {
-        [HarmonyPrefix]
-        public static bool Prefix(ref bool __result)
+        private static readonly FieldInfo SquelchedField = AccessTools.Field(typeof(EnemyEmoteHandler), "m_squelched");
+
+        private static EnemyEmoteHandler appliedTo;
+
+        [HarmonyPostfix]
+        [HarmonyPatch("Awake")]
+        public static void AwakePostfix(EnemyEmoteHandler __instance)
         {
-            Plugin.Logger.LogInfo($"Patching IsSquelched: {__result}");
-            __result = true;
-            Plugin.Logger.LogInfo($"Returning instead: {__result}");
-            return false;
+            TryApply(__instance);
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch("IsSquelched", typeof(int))]
+        public static void IsSquelchedPrefix(EnemyEmoteHandler __instance)
+        {
+            TryApply(__instance);
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch("ShowEmotes")]
+        public static void ShowEmotesPrefix(EnemyEmoteHandler __instance)
+        {
+            TryApply(__instance);
+        }
+
+        private static void TryApply(EnemyEmoteHandler handler)
+        {
+            if (handler == null || appliedTo == handler)
+            {
+                return;
+            }
+
+            if (SquelchedField == null)
+            {
+                Plugin.Logger.LogError("EnemyEmoteHandler.m_squelched was not found; auto-squelch was not applied");
+                return;
+            }
+
+            object squelched = SquelchedField.GetValue(handler);
+            if (squelched == null)
+            {
+                return;
+            }
+
+            GameState gameState = GameState.Get();
+            GameMgr gameMgr = GameMgr.Get();
+            if (gameState == null || gameMgr == null)
+            {
+                return;
+            }
+
+            if (gameMgr.IsBattlegrounds())
+            {
+                int localPlayerId = gameState.GetFriendlyPlayerId();
+                if (localPlayerId == 0)
+                {
+                    return;
+                }
+
+                foreach (int playerId in SnapshotKeys(squelched))
+                {
+                    if (playerId != localPlayerId)
+                    {
+                        SetSquelched(squelched, playerId, true);
+                    }
+                }
+            }
+            else
+            {
+                int opposingPlayerId = gameState.GetOpposingPlayerId();
+                if (opposingPlayerId == 0 || !ContainsPlayer(squelched, opposingPlayerId))
+                {
+                    return;
+                }
+
+                SetSquelched(squelched, opposingPlayerId, true);
+            }
+
+            appliedTo = handler;
+            Plugin.Logger.LogInfo("Auto-squelched opponents for this game");
+        }
+
+        private static List<int> SnapshotKeys(object squelched)
+        {
+            var keys = new List<int>();
+            var keyCollection = squelched.GetType().GetProperty("Keys")?.GetValue(squelched) as IEnumerable;
+            if (keyCollection == null)
+            {
+                return keys;
+            }
+
+            foreach (object key in keyCollection)
+            {
+                keys.Add((int)key);
+            }
+
+            return keys;
+        }
+
+        private static bool ContainsPlayer(object squelched, int playerId)
+        {
+            return (bool)squelched.GetType().GetMethod("ContainsKey").Invoke(squelched, new object[] { playerId });
+        }
+
+        private static void SetSquelched(object squelched, int playerId, bool value)
+        {
+            squelched.GetType().GetProperty("Item").SetValue(squelched, value, new object[] { playerId });
         }
     }
 }
